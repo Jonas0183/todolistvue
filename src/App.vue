2c2e9d4e31b05778d28edf08e6d55d40b4531466
Jonas <jonasperezmira@gmail.com>
2020-12-03 16:47:33 +0100

    Proyecto creado y componente del head con el nombre de la app añadido

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Head/>
   </div>
 </template>
 
+<script>
+import Head from '@/components/Head.vue';
+
+export default {
+  name: 'App',
+  components:{
+    Head,
+  }
+};
+</script>
 <style lang="scss">
 #app {
+  height: 100%;
+  background-color: #a3837f;
+  max-width:1200px;
+  margin-top: 1vh;
+  margin: auto;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Head.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+       <h1> To Do List App </h1> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Head'
+}
+</script>
+
+<style lang="scss">
+    h1{
+        font-size: 4vw;
+        color: #7a1003;
+        background-color: gray;
+   }
+</style>
